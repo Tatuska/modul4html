731b1712e959a2c53c8ff2b585c49a97487075b3
Tatuska <ttevik@gmail.com>
2017-01-03 20:23:45 +0100

    new

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -7,7 +7,7 @@
   var byeSpeaker={};
   var speakWord = "Good Bye";
 
-  byeSpeaker.speak=function () {
+  byeSpeaker.speak=function (name) {
     console.log(speakWord + " " + name);
   }
   window.byeSpeaker=byeSpeaker;
